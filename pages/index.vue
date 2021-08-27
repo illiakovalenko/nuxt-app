4e24a655c1be1185ba00ddd4ec6957911c504c47
illiakovalenko <zlatoborodyi@gmail.com>
2021-08-27 12:33:33 +0300

    Simple page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,78 @@
 <template>
-  <Tutorial/>
+  <div id="test">
+    <h2>It's a Nuxt.js application</h2>
+    <h3>Antd framework is enabled</h3>
+    <a-carousel arrows dots-class="slick-dots slick-thumb">
+      <a slot="customPaging" slot-scope="props">
+        <img :src="getImgUrl(props.i)" />
+      </a>
+      <div v-for="item in 4">
+        <img :src="baseUrl + 'abstract0' + item + '.jpg'" />
+      </div>
+    </a-carousel>
+    <div>
+      <a-card title="Default size card" style="width: 300px">
+        <a slot="extra" href="#">more</a>
+        <p>card content</p>
+        <p>card content</p>
+        <p>card content</p>
+      </a-card>
+      <br />
+      <a-card size="small" title="Small size card" style="width: 300px">
+        <a slot="extra" href="#">more</a>
+        <p>card content</p>
+        <p>card content</p>
+        <p>card content</p>
+      </a-card>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+const baseUrl =
+  'https://raw.githubusercontent.com/vueComponent/ant-design-vue/master/components/vc-slick/assets/img/react-slick/'
+export default Vue.extend({
+  data() {
+    return {
+      baseUrl,
+    }
+  },
+  methods: {
+    getImgUrl(i) {
+      return `${baseUrl}abstract0${i + 1}.jpg`
+    },
+  },
+})
 </script>
+<style scoped>
+#test {
+  padding: 30px;
+}
+/* For demo */
+.ant-carousel >>> .slick-dots {
+  height: auto;
+}
+.ant-carousel >>> .slick-slide img {
+  border: 5px solid #fff;
+  display: block;
+  margin: auto;
+  max-width: 80%;
+}
+.ant-carousel >>> .slick-thumb {
+  bottom: -45px;
+}
+.ant-carousel >>> .slick-thumb li {
+  width: 60px;
+  height: 45px;
+}
+.ant-carousel >>> .slick-thumb li img {
+  width: 100%;
+  height: 100%;
+  filter: grayscale(100%);
+}
+.ant-carousel >>> .slick-thumb li.slick-active img {
+  filter: grayscale(0%);
+}
+</style>
